1e7924942155f686563a483c50f038eecca8d69c
Joe <joe@Joes-MacBook-Pro-2.local>
2019-04-28 17:26:49 +0100

    use ring-json

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :min-lein-version "2.0.0"
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
-                 [cheshire "5.8.1"]
+                 [ring/ring-json "0.4.0"]
                  [ring/ring-defaults "0.3.2"]]
   :plugins [[lein-ring "0.12.5"]]
   :ring {:handler cheshire-cat.handler/app}
