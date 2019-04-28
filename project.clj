3c5ced90d5b349c041f8134585bb7d504f2d491d
Joe <joe@Joes-MacBook-Pro-2.local>
2019-04-28 20:57:36 +0100

    add cljs and html serving

--- project.clj
+++ project.clj
@@ -5,9 +5,18 @@
   :dependencies [[org.clojure/clojure "1.10.0"]
                  [compojure "1.6.1"]
                  [ring/ring-json "0.4.0"]
+                 [org.clojure/clojurescript "1.10.520"]
                  [ring/ring-defaults "0.3.2"]]
-  :plugins [[lein-ring "0.12.5"]]
+  :plugins [[lein-ring "0.12.5"]
+            [lein-cljsbuild "1.1.7"]]
   :ring {:handler cheshire-cat.handler/app}
   :profiles
   {:dev {:dependencies [[javax.servlet/servlet-api "2.5"]
-                        [ring/ring-mock "0.3.2"]]}})
+                        [ring/ring-mock "0.3.2"]]}}
+  :cljsbuild {
+    :builds [{
+        :source-paths ["src-cljs"]
+        :compiler {
+          :output-to "resources/public/main.js" 
+          :optimizations :whitespace
+          :pretty-print true}}]})
